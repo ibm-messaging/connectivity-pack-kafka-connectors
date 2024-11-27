f439f73b8086b09c4355221c9e42494803d12fa2
Varada Sunanda <Varada.Sunanda@ibm.com>
2024-11-27 21:11:41 +0530

    feat: Update the jenkins job for helm connectivity pack (#41)
    Contributes to: https://jsw.ibm.com/browse/EVI-7176
    
    Signed-off-by: Varada Sunanda <Varada.Sunanda@ibm.com>

--- build/build_and_publish.sh
+++ build/build_and_publish.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+rm -rf ibm-connectivity-pack/
+
 . ${0%/*}/fetch_latest_helm_chart.sh
 . ${0%/*}/update_docker_files_with_latest_digest.sh
 . ${0%/*}/build_images.sh
 . ${0%/*}/update_image_config_with_latest_digest.sh
+
+cp -r ea_files/helm-templates/* ibm-connectivity-pack/templates
+cp -r ea_files/docs/* ibm-connectivity-pack/
 
 # Remove existing licenses under root directory and helm chart
 rm -rf license
